f4da84af5ad0d044dc628765a8fa4c3edb11bd16
Filip Strajnar <filip.strajnar@gmail.com>
2023-05-28 11:57:17 +0200

    Nicer JSON structure

--- src/matrix_cloud/__main__.py
+++ src/matrix_cloud/__main__.py
@@ -2,6 +2,7 @@ import simple_matrix_api
 from simple_matrix_api.client import Client
 import argparse
 import json
+from os import path
 
 READ_SIZE = 50_000_000
 
@@ -9,35 +10,50 @@ READ_SIZE = 50_000_000
 def upload_large_file(
     username: str,
     password: str,
-    file_input: str,
-    file_output: str,
+    file_path: str,
     matrix_url: str,
 ):
+    if not path.isfile(file_path):
+        print("Argument file_path doesn't represent a file.")
+        return
+
     client: Client = simple_matrix_api.login(username, password, matrix_url)
     urls: list[str] = []
-    with open(file_input, "rb") as input:
+
+    with open(file_path, "rb") as input:
         while True:
             content = input.read(READ_SIZE)
             if (len(content) == 0):
                 break
             urls.append(client.upload("_", content))
-    with open(file_output, "w") as output:
-        json.dump(urls, output)
+
+    file_name = path.basename(file_path)
+    json_path = f"{file_name}.json"
+    with open(json_path, "w") as output:
+        json.dump({"file_name": file_name, "urls": urls}, output)
 
 
 def download_large_file(
     username: str,
     password: str,
-    file_input: str,
-    file_output: str,
+    file_path: str,
     matrix_url: str,
 ):
+    if not path.isfile(file_path):
+        print("Argument 'file_path' doesn't represent a file.")
+        return
+
+    if not file_path.endswith(".json"):
+        print(
+            "Warning: file_path expects json, but file doesn't end with '.json'."
+        )
+
     client: Client = simple_matrix_api.login(username, password, matrix_url)
 
-    with open(file_input, "r") as input:
-        urls: list[str] = json.load(input)
-        with open(file_output, "wb") as output:
-            for url in urls:
+    with open(file_path, "r") as input:
+        json_data = json.load(input)
+        with open(json_data["file_name"], "wb") as output:
+            for url in json_data["urls"]:
                 content_part = client.get_file(url)
                 if content_part != None:
                     output.write(content_part)
@@ -46,8 +62,7 @@ def download_large_file(
 parser = argparse.ArgumentParser()
 parser.add_argument("username")
 parser.add_argument("password")
-parser.add_argument("file_input")
-parser.add_argument("file_output")
+parser.add_argument("file_path")
 parser.add_argument("-m", "--matrix")
 parser.add_argument("-d", "--download", action='store_true')
 args = parser.parse_args()
@@ -56,10 +71,10 @@ matrix_address = args.matrix if args.matrix != None else "matrix.org"
 
 if args.download:
     print("Downloading")
-    download_large_file(args.username, args.password, args.file_input,
-                        args.file_output, matrix_address)
+    download_large_file(args.username, args.password, args.file_path,
+                        matrix_address)
 
 else:
     print("Uploading")
-    upload_large_file(args.username, args.password, args.file_input,
-                      args.file_output, matrix_address)
+    upload_large_file(args.username, args.password, args.file_path,
+                      matrix_address)
